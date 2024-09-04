8cbee4dc2cbe1153d877ccf4b5d7253dfbe10db4
sveta-bezradina <118204678+sveta-bezradina@users.noreply.github.com>
2024-09-04 18:34:40 +0300

    Laboratorna №1

---
+++ Lr_1/Lr_1/Program.cs
@@ -0,0 +1,27 @@
+﻿var builder = WebApplication.CreateBuilder();
+
+var app = builder.Build();
+
+
+Company c1 = new Company("The Coca-Cola", "The Coca-Cola Company and Coca-Cola HBC have been working, investing and supporting Ukrainian society for 30 years.");
+
+app.MapGet("/", async context => await context.Response.WriteAsync(c1.ToString()));
+
+Random rnd = new Random();
+app.MapGet("/numb", () => rnd.Next(1, 101).ToString());
+app.Run();
+
+class Company
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public Company(string name, string descr) { 
+        Name = name;
+        Description = descr;
+    }
+    public override string ToString()
+    {
+        return "Name: " + Name.ToString() + " Description: " + Description.ToString();
+    }
+}

03136c8b86692509fd9d233d31b6f0387e5a1e94
sveta-bezradina <118204678+sveta-bezradina@users.noreply.github.com>
2024-11-07 23:25:59 +0200

    Add files via upload

---
+++ Controllers/FileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace lr_7.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(fileName))
+            {
+                ViewData["Error"] = "Всі поля повинні бути заповнені.";
+                return View();
+            }
+
+            var content = $"Ім'я: {firstName}\nПрізвище: {lastName}";
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), $"{fileName}.txt");
+            System.IO.File.WriteAllText(filePath, content);
+
+            TempData["SuccessMessage"] = "Файл успішно створено!";
+
+            return RedirectToAction("DownloadFile", "File");
+        }
+    }
+}

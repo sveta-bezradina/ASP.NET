d8ca9445dcd30f0d3f7dc7c7cbe73115bbe6cb7e
sveta-bezradina <118204678+sveta-bezradina@users.noreply.github.com>
2024-11-15 14:04:39 +0200

    Add files via upload

---
+++ Attributes/CustomDateValidationAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lr_10.Attributes
+{
+    public class CustomDateValidationAttribute: ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime date)
+            {
+                if (date <= DateTime.Now)
+                {
+                    return false;
+                }
+
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Controllers/ConsultationController.cs
@@ -0,0 +1,36 @@
+﻿using lr_10.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lr_10.Controllers
+{
+    public class ConsultationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegistrationViewModel model)
+        {
+            if (!model.IsValidConsultationDate())
+            {
+                ModelState.AddModelError("ConsultationDate", "Консультація щодо продукту 'Основи' не може проходити по понеділках.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Success");
+            }
+
+            return View(model);
+        }
+
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
